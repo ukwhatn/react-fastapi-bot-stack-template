4775962b818a1cd36daa91a45ad8a4c804d20ad5
Yuki Watanabe <ukwhatn@gmail.com>
2023-02-07 04:30:15 +0900

    update

--- development.sh
+++ development.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+# frontend init
+docker-compose build
+docker compose run --rm frontend sh -c "create-react-app app --template typescript"
+
+# python requirements
 pip install -r backend/requirements.txt
 pip install -r db/requirements.txt
 pip install -r bot/requirements.txt
